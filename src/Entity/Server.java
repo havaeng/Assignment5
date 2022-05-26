249406dc9ed57841761b2dde10e6d270b13223e3
ElisabetAronsson <96416333+ElisabetAronsson@users.noreply.github.com>
2022-05-26 15:19:15 +0200

    Random

--- src/Entity/Server.java
+++ src/Entity/Server.java
@@ -8,6 +8,7 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 public class Server{
